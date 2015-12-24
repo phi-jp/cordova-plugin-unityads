add66e42fedad1844850689a2125cc34aed58317
phi <phi1618jp@gmail.com>
2015-12-24 18:02:47 +0900

    fix android

--- src/android/UnityAds.java
+++ src/android/UnityAds.java
@@ -4,3 +4,21 @@
 
 
 package jp.phi.cordova.plugin.unityads;
+
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+
+import com.unity3d.ads.android.UnityAds;
+
+public class UnityAds extends CordovaPlugin {
+
+    public UnityAds() {
+
+    }
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+}
