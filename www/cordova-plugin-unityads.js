60ffa60b36974fa1c9c5746c9e48aa7e2f956e88
phi <phi1618jp@gmail.com>
2015-12-24 14:48:17 +0900

    fix www

--- www/cordova-plugin-unityads.js
+++ www/cordova-plugin-unityads.js
@@ -4,5 +4,13 @@ module.exports = {
   init: function(gameId, isTest, success, error) {
     isTest = (isTest !== undefined) isTest : true;
     exec(success, error, "UnityAds", "init", [gameId, isTest]);
-  }
+  },
+
+  showVideoAd: function() {
+    exec(success, error, "UnityAds", "showVideoAd");
+  },
+
+  showRewardedVideoAd: function() {
+    exec(success, error, "UnityAds", "showRewardedVideoAd");
+  },
 };
