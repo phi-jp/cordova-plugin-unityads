c81e2639720cb93d829d14b7fe9c8f809bd0cf5a
phi <phi1618jp@gmail.com>
2015-12-24 15:18:44 +0900

    bugfix

--- www/cordova-plugin-unityads.js
+++ www/cordova-plugin-unityads.js
@@ -2,7 +2,7 @@ var exec = require('cordova/exec');
 
 module.exports = {
   init: function(gameId, isTest, success, error) {
-    isTest = (isTest !== undefined) isTest : true;
+    isTest = (isTest !== undefined) ? isTest : true;
     exec(success, error, "UnityAds", "init", [gameId, isTest]);
   },
 
