252f7ea3ea8481b6ee5c736fc134da5052c46c7a
phi <phi1618jp@gmail.com>
2015-12-25 13:50:19 +0900

    fix android

--- src/android/UnityAdsPlugin.java
+++ src/android/UnityAdsPlugin.java
@@ -14,9 +14,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.unity3d.ads.android.IUnityAdsListener;
 import com.unity3d.ads.android.UnityAds;
 
-public class UnityAdsPlugin extends CordovaPlugin {
+
+public class UnityAdsPlugin extends CordovaPlugin implements IUnityAdsListener {
 
     protected String gameId;
     protected boolean isTest;
@@ -30,11 +32,22 @@ public class UnityAdsPlugin extends CordovaPlugin {
     }
 
     private void init(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        UnityAds.setTestMode(isTest);
+        final String gameId = args.getString(0);
+        final Boolean isTest = args.getBoolean(1);
+
+        this.gameId = gameId;
+        this.isTest = isTest;
+
+        UnityAds.init(cordova.getActivity(), this.gameId, this);
+        UnityAds.setTestMode(this.isTest);
+        UnityAds.setDebugMode(false);
+//        UnityAds.setZone("rewardedVideoZone");
     }
 
-    private void showAdd(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        UnityAds.setTestMode(isTest);
+    private void showAds(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if(UnityAds.canShow() && UnityAds.canShowAds()) {
+            UnityAds.show();
+        }
     }
 
     @Override
@@ -42,14 +55,57 @@ public class UnityAdsPlugin extends CordovaPlugin {
         if (action.equals("init")) {
             init(action, args, callbackContext);
             return true;
-        }
-        else if (action.equals("showAdd")){
-            showAdd(action, args, callbackContext);
+        } else if (action.equals("showAds")){
+            showAds(action, args, callbackContext);
             return true;
         }
 
         // method not found
         return false;
+    }
+
+    @Override
+    /**
+     * 動画在庫がある場合のコールバック
+     */
+    public void onFetchCompleted() {
+    }
+
+    @Override
+    /**
+     * 動画在庫がない場合のコールバック
+     */
+    public void onFetchFailed() {
+    }
+
+    @Override
+    /**
+     * UnityAdsが表示されたコールバック
+     */
+    public void onHide() {
+    }
+
+    @Override
+    /**
+     * UnityAdsが閉じられたコールバック
+     */
+    public void onShow() {
+    }
+
+    @Override
+    /**
+     * 動画の再生開始コールバック
+     */
+    public void onVideoStarted() {
+    }
+
+    @Override
+    /**
+     * 動画の再生完了コールバック
+     * @param key インセンティブキー
+     * @param skip スキップしたか
+     */
+    public void onVideoCompleted(String key, boolean skip) {
     }
 
 }
