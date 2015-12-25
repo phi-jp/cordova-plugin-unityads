9ce45af0244e4d9e6ac5247fa8503249d63ba2f6
phi <phi1618jp@gmail.com>
2015-12-25 13:28:57 +0900

    fix android

--- src/android/UnityAdsPlugin.java
+++ src/android/UnityAdsPlugin.java
@@ -10,15 +10,46 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.unity3d.ads.android.UnityAds;
 
 public class UnityAdsPlugin extends CordovaPlugin {
 
-    public UnityAds() {
+    protected String gameId;
+    protected boolean isTest;
+
+    public UnityAdsPlugin() {
 
     }
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
     }
+
+    private void init(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        UnityAds.setTestMode(isTest);
+    }
+
+    private void showAdd(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        UnityAds.setTestMode(isTest);
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("init")) {
+            init(action, args, callbackContext);
+            return true;
+        }
+        else if (action.equals("showAdd")){
+            showAdd(action, args, callbackContext);
+            return true;
+        }
+
+        // method not found
+        return false;
+    }
+
 }
